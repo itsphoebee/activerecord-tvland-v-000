8677a82db84e759285b02ea085485124738cded1
Phoebe Lau <phoebelau7@gmail.com>
2018-01-04 21:35:26 +0000

    automatically backed up by learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -7,6 +7,8 @@ class Actor < ActiveRecord::Base
   end
 
   def list_roles
-    "#{self.characters.name}"
+    self.characters.each do |character|
+      "#{character.name} - #{character.show}"
+    end
   end
 end
