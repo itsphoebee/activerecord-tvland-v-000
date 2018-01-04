d39a4be681cb30c3fa8fcefdf09f1f7872d3db30
Phoebe Lau <phoebelau7@gmail.com>
2018-01-04 21:38:32 +0000

    automatically backed up by learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -7,7 +7,7 @@ class Actor < ActiveRecord::Base
   end
 
   def list_roles
-    characters.tap do |character|
+    characters.map do |character|
       "#{character.name} - #{character.show.name}"
     end
   end
